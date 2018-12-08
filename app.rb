26f86978f4e3d6808988c1de71526ee624dc8b3f
JordanRushing <rushing.jordan@gmail.com>
2018-12-08 18:46:25 +0000

    Add Rubocop dependency; Create Rubocop config; Exec Rubocop on existing code; Update app.rb with comments, ordered dependencies, convert token to instance variable

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 # app.rb
-require 'roda'
-require 'json'
 require 'chillbud'
+require 'json'
+require 'roda'
 require_relative './lib/chillbud/jobs/chillbud_job'
 
+# Request routing tree child class
 class ChillbudApp < Roda
+  # Roda plugin initializations:
+  # https://github.com/jeremyevans/roda/tree/master/lib/roda/plugins
   plugin :json
 
+  # Start/Stop request route
   route do |r|
     # /api
     r.on 'api' do
@@ -15,31 +19,27 @@ class ChillbudApp < Roda
         # /api/v1/start
         r.on 'start' do
           r.post do
-            discord_token = r.params.fetch('discord_token')
+            @discord_token = r.params.fetch('discord_token')
             Chillbud::Jobs::ChillbudJob.perform_async(
-              {
-                action: :start,
-                discord_token: discord_token,
-              }
+              action: :start,
+              discord_token: @discord_token
             )
             {
-              message: "Successfully started bot",
-              discord_token: discord_token
+              message: 'Successfully started bot',
+              discord_token: @discord_token
             }
           end
         end
         r.on 'stop' do
           r.post do
-            discord_token = r.params.fetch('discord_token')
+            @discord_token = r.params.fetch('discord_token')
             Chillbud::Jobs::ChillbudJob.perform_async(
-              {
-                action: :stop,
-                discord_token: discord_token
-              }
+              action: :stop,
+              discord_token: @discord_token
             )
             {
-              message: "Successfully stopped bot",
-              discord_token: discord_token
+              message: 'Successfully stopped bot',
+              discord_token: @discord_token
             }
           end
         end
--- chillbud.gemspec
+++ chillbud.gemspec
@@ -43,4 +43,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "rack-test"
   spec.add_development_dependency "pry"
+  spec.add_development_dependency "rubocop"
 end
